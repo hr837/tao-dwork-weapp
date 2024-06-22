0969714fb958b901856f7c0bc2a1083e89d02fdb
卫涛 <wt_yidingdian@126.com>
2024-06-22 16:02:42 +0800

    创建企业，调用服务添加

--- miniprogram/pages/company/create/create.ts
+++ miniprogram/pages/company/create/create.ts
@@ -1,6 +1,9 @@
 // pages/company/create/create.ts
 import Message from '../../../miniprogram_npm/tdesign-miniprogram/message/index';
 
+import DIC_STORAGE_API from '../../../storages/dic-storage';
+import tenantService from "../../../services/tenant-service";
+
 Page({
 
   /**
@@ -18,9 +21,9 @@ Page({
     areaText: "",
     areaPickerVisable: false,
     industryOptions: [
-      { label: '互联网', value: 'HLW' },
-      { label: '制造业', value: 'ZZY' },
-      { label: '金融业', value: 'JRY' }
+      { label: '互联网', value: 1 },
+      { label: '制造业', value: 2 },
+      { label: '金融业', value: 3 }
     ], // 示例选项
     areaOptions: [
       { label: '华北', value: 'HB' },
@@ -29,10 +32,24 @@ Page({
       { label: '西北', value: 'XB' }
     ],
     scaleOptions: [ // 人员规模选项
-      { label: '1-50人', value: 'S' },
-      { label: '51-100人', value: 'M' },
-      { label: '101-500人', value: 'L' },
+      { label: '1-50人', value: 1 },
+      { label: '51-100人', value: 2 },
+      { label: '101-500人', value: 3 },
     ],
+  },
+
+  onLoad(_options: any) {
+
+    // 所属行业字典
+    DIC_STORAGE_API.getDicByCategoryCode('sshy_type').then((_res: Array<any>) => {
+      this.setData({ industryOptions: _res })
+    });
+
+    // 人员规模字典
+    DIC_STORAGE_API.getDicByCategoryCode('rygm_type').then((_res: Array<any>) => {
+      this.setData({ scaleOptions: _res })
+    });
+
   },
 
   // 企业名称变化，更新data
@@ -44,14 +61,29 @@ Page({
 
   // 用户点击提交
   handleSubmit() {
-    if(!this.data.name){
+    if (!this.data.name) {
       Message.error({
         context: this,
-        closeBtn:true,
+        closeBtn: true,
         content: '请填写企业名称',
       });
       return;
     }
+
+    tenantService.add({
+      // 名称
+      name: this.data.name,
+      // 区域
+      area: this.data.area && this.data.area.length ? this.data.area[0] : '',
+      // 所属行业
+      industry: this.data.industry && this.data.industry.length ? +this.data.industry[0] : 0,
+      // 企业规模
+      staffSize: this.data.scale && this.data.scale.length ? +this.data.scale[0] : 0,
+    }).then((_res: any) => {
+      if (_res.code == 200) {
+
+      }
+    })
   },
 
   //#region area 相关
@@ -98,6 +130,4 @@ Page({
     })
   },
   //#endregion
-
-
 })
