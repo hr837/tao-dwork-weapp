e4b8cb3540a23fff2db77fa9ca58675c3ac09c39
HuangRui <hr837@163.com>
2024-06-22 23:04:16 +0800

    feat: 我的页面

---
+++ miniprogram/pages/mine/zone/zone.ts
@@ -0,0 +1,73 @@
+// pages/mine/zone/zone.ts
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    /* 任务数量 */
+    taskCount: 0,
+    /* 用户头像 */
+    avatarImage: '/assets/images/avatar-empty.png',
+    /* 通知显示标识 */
+    noticeVisable: true,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+    // TODO load task count
+  },
+  // 用户点击退出
+  onLoginOut() {
+
+  },
+  // 用户点击认证
+  onAuthenticate() {
+
+  },
+  // 用户点击关于
+  onAbout() {
+
+  },
+  // 用户点击意见反馈
+  onFeedback() {
+
+  },
+  // 用户点击我的企业
+  onCompany() {
+
+  },
+  // 用户点击二维码
+  onQrCode(){
+
+  },
+  // 我参与的
+  onJoined(){
+
+  },
+  // 待审核
+  onApprove(){
+
+  },
+  // 个人信息
+  onUserInfo(){
+    
+  }
+})
